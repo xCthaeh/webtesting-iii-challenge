60e928928707defe954f9610465af0aafe7452f1
Alyssa Hatfield <xcthaeh@gmail.com>
2019-06-19 14:30:59 -0400

    finished writing tests for display

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,28 +1,55 @@
 // Test away
 import React from "react";
 import renderer from "react-test-renderer";
-import { render, fireEvent, cleanup } from '@testing-library/react'
-
+import { render, fireEvent, cleanup } from "@testing-library/react";
 import Dashboard from "./Dashboard";
 
-describe("<Dashboard />", () => {
+describe("<Display />", () => {
+    
   it("should match snapshot", () => {
-    const tree = renderer.create(<Dashboard />).toJSON();
-
+    const tree = renderer.create(<Display />).toJSON();
     expect(tree).toMatchSnapshot();
   });
+
   it("should render", () => {
     render(<Display closed="true" locked="true" />);
-    cleanup();
   });
+
+  it("should default to unlocked", () => {
+    const { getByText } = render(<Display />);
+    getByText(/unlocked/i);
+  });
+
+  it("should default to open", () => {
+    const { getByText } = render(<Display />);
+    getByText(/open/i);
+  });
+
   it("should be locked", () => {
-    const { getByText } = render(<Display closed="true" locked="true" />);
+    const { getByText } = render(<Display closed={true} locked={true} />);
     getByText(/locked/i);
-    cleanup();
   });
+
   it("should be closed", () => {
-    const { getByText } = render(<Display closed="true" locked="true" />);
+    const { getByText } = render(<Display closed={true} locked={true} />);
     getByText(/closed/i);
-    cleanup();
   });
+
+  it("should be unlocked", () => {
+    const { getByText } = render(<Display closed={false} locked={false} />);
+    getByText(/unlocked/i);
+  });
+
+  it("should be green-led", () => {
+    const { getByText } = render(<Display closed={false} locked={false} />);
+    expect(getByText(/open/i).classList[1]).toEqual("green-led");
+    expect(getByText(/unlocked/i).classList[1]).toEqual("green-led");
+  });
+
+  it("should be red-led", () => {
+    const { getByText } = render(<Display closed={true} locked={true} />);
+    expect(getByText(/closed/i).classList[1]).toEqual("red-led");
+    expect(getByText(/locked/i).classList[1]).toEqual("red-led");
+  });
+
 });
