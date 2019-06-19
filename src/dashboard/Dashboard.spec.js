9c98f054a11fdc1929223ef7fe87affd4ac262a1
Alyssa Hatfield <xcthaeh@gmail.com>
2019-06-19 14:22:06 -0400

    testing dash component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,13 @@
 // Test away
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Dashboard from "./Dashboard";
+
+describe("<Dashboard />", () => {
+  it("should match snapshot", () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
