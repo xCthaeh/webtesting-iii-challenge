f8166a349e19c2e845c6aa54b62d9d3817219766
Alyssa Hatfield <xcthaeh@gmail.com>
2019-06-19 14:24:17 -0400

    added a check for locked and closed

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,6 +1,7 @@
 // Test away
 import React from "react";
 import renderer from "react-test-renderer";
+import { render, fireEvent, cleanup } from '@testing-library/react'
 
 import Dashboard from "./Dashboard";
 
@@ -9,5 +10,19 @@ describe("<Dashboard />", () => {
     const tree = renderer.create(<Dashboard />).toJSON();
 
     expect(tree).toMatchSnapshot();
+  });
+  it("should render", () => {
+    render(<Display closed="true" locked="true" />);
+    cleanup();
+  });
+  it("should be locked", () => {
+    const { getByText } = render(<Display closed="true" locked="true" />);
+    getByText(/locked/i);
+    cleanup();
+  });
+  it("should be closed", () => {
+    const { getByText } = render(<Display closed="true" locked="true" />);
+    getByText(/closed/i);
+    cleanup();
   });
 });
