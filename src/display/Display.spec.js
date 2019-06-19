0da795d77f1fffb1b1a3389d231684187f5bdf9f
Alyssa Hatfield <xcthaeh@gmail.com>
2019-06-19 14:00:30 -0400

    fixed dependencies, testing display component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,14 @@
 // Test away!
+
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Display from "./Display";
+
+describe("<Display />", () => {
+  it("should match snapshot", () => {
+    const tree = renderer.create(<Display />).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
